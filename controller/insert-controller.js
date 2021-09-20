24aad5ad39671c4e06b18cef6feb668a2649ed58
Dinesh <pr760677@gmail.com>
2021-09-21 00:25:12 +0530

    changed structure of  todos array in database ,add 1 todo at 1 one time,,updateone for new todo and existing todo check

--- controller/insert-controller.js
+++ controller/insert-controller.js
@@ -20,9 +20,12 @@ module.exports = {
 
     addUser: function (req, res) {
         //checking input
+        var mydate = {
+            "_id": `${req.body.id}`
+        }
 
         var tododata = {
-            "id": `${req.body.id}`,
+            "_id": `${req.body.id}`,
             "todos": [
                 {
                     "from": `${req.body.s1_from}`,
@@ -30,85 +33,53 @@ module.exports = {
                     "name": `${req.body.s1_title}`,
                     "description": `${req.body.s1_dec}`
 
-                },
-                {
-                    "from": `${req.body.s2_from}`,
-                    "to": `${req.body.s2_to}`,
-                    "name": `${req.body.s2_title}`,
-                    "description": `${req.body.s2_dec}`
-
-                },
-                {
-                    "from": `${req.body.s3_from}`,
-                    "to": `${req.body.s3_to}`,
-                    "name": `${req.body.s3_title}`,
-                    "description": `${req.body.s3_dec}`
-
-
-                },
-                {
-                    "from": `${req.body.s4_from}`,
-                    "to": `${req.body.s4_to}`,
-                    "name": `${req.body.s4_title}`,
-                    "description": `${req.body.s4_dec}`
-
-                },
-                {
-                    "from": `${req.body.s5_from}`,
-                    "to": `${req.body.s5_to}`,
-                    "name": `${req.body.s5_title}`,
-                    "description": `${req.body.s5_dec}`
-
-                },
-                {
-                    "from": `${req.body.s6_from}`,
-                    "to": `${req.body.s6_to}`,
-                    "name": `${req.body.s6_title}`,
-                    "description": `${req.body.s6_dec}`
-
-                },
-                {
-                    "from": `${req.body.s7_from}`,
-                    "to": `${req.body.s7_to}`,
-                    "name": `${req.body.s7_title}`,
-                    "description": `${req.body.s7_dec}`
-
-                },
-                {
-                    "from": `${req.body.s8_from}`,
-                    "to": `${req.body.s8_to}`,
-                    "name": `${req.body.s8_title}`,
-                    "description": `${req.body.s8_dec}`
-
-                },
-                {
-                    "from": `${req.body.s9_from}`,
-                    "to": `${req.body.s9_to}`,
-                    "name": `${req.body.s9_title}`,
-                    "description": `${req.body.s9_dec}`
-
                 }
             ]
         }
 
 
+        db.collection('doto').updateOne(
+            mydate,
+            {
+                $push: {
+                    todos: {
+                        $each: [{
+                            "from": `${req.body.s1_from}`,
+                            "to": `${req.body.s1_to}`,
+                            "name": `${req.body.s1_title}`,
+                            "description": `${req.body.s1_dec}`
+                        }]
+                    }
+                }
+            },
+            {
+                upsert: true,
 
 
-        db.collection('doto').insertOne(tododata, function (err, result) {
-            if (err) {
-                console.log("Error While Adding the data");
+            }, function (err, re) {
+                if (err) {
+                    console.log("Error While Adding the data");
 
-                return ('Error');
+                    return ('Error');
+                }
+                else {
+                    res.render('success')
+                    console.log("Todo Added")
+                }
+
+
             }
 
-
-            else {
-                res.render('success')
-                console.log("Todo Added")
-            }
+        )
 
 
-        });
+
+
+
+
+
+
+
 
     },
 
@@ -120,9 +91,9 @@ module.exports = {
         } else {
 
             var mySearch = {
-                'id': search_term
+                '_id': search_term
             }
-       
+
 
             //Find One for ONly one result
             db.collection('doto').findOne(mySearch, (err, result) => {
@@ -131,7 +102,7 @@ module.exports = {
                 }
                 else {
                     if (result == null) {
-                        
+
                         res.render('err2');
 
                     } else {
